64da5e7db6f735b0c57842bdd51ab60a269474b7
PIBO0121044 <PIBO0121044@CL329-02>
2023-09-16 17:04:43 +0700

    Добавьте файлы проекта.

---
+++ Laba 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите сторону a :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("введите сторону b :");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int S = a * b;
+            int P = 2 * (a + b);
+            Console.WriteLine($"S = {S}  P = {P}");
+
+            Console.ReadKey ();
+
+
+        }
+    }
+}
